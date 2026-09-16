6d9b41cce5f434eae8f7539eec3edd10b830e1aa
infaan-hub <salehabdulrahim8587@gmail.com>
2026-09-16 16:56:39 +0300

    Add debug/media endpoint to diagnose MEDIA_ROOT on Render + fix MEDIA_ROOT to string

--- backend/zda/zda/settings.py
+++ backend/zda/zda/settings.py
@@ -103,7 +103,7 @@ USE_I18N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.parent / 'media'
+MEDIA_ROOT = str(BASE_DIR.parent / 'media')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
--- backend/zda/zda/urls.py
+++ backend/zda/zda/urls.py
@@ -3,10 +3,28 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve as static_serve
+import os
+
+def debug_media(request):
+    from django.http import JsonResponse
+    mr = settings.MEDIA_ROOT
+    files = []
+    gallery_dir = os.path.join(mr, 'designs', 'gallery')
+    if os.path.isdir(gallery_dir):
+        files = os.listdir(gallery_dir)[:5]
+    return JsonResponse({
+        'MEDIA_ROOT': mr,
+        'MEDIA_URL': settings.MEDIA_URL,
+        'DEBUG': settings.DEBUG,
+        'gallery_exists': os.path.isdir(gallery_dir),
+        'gallery_files': files,
+        'gallery_count': len(os.listdir(gallery_dir)) if os.path.isdir(gallery_dir) else 0,
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('designs_app.urls')),
+    path('debug/media/', debug_media),
 ]
 
 if settings.DEBUG:
